25fbc9305196d8f62be6f35d43280f4cba1969b1
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-20 22:56:10 +0800

    comments added

--- momentum.py
+++ momentum.py
@@ -2,10 +2,14 @@ import configparser
 import oandapy
 import pandas
 
+# loading config variables from oanda.cfg file. Please see oanda_example.cfg.
 config = configparser.ConfigParser()
 config.read('oanda.cfg')
 
+# Initializing Oanda API with config oath information
 oanda = oandapy.API(environment='practice',
                     access_token=config['oanda']['access_token'])
+
+
 
 
