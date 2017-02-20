e551f71abcf815e042e58b5c02dadcd2b459da8a
HintikkaKimmo <hintikkakimmo@gmail.com>
2017-02-20 23:04:56 +0800

    oanda now loads data

--- momentum.py
+++ momentum.py
@@ -10,6 +10,15 @@ config.read('oanda.cfg')
 oanda = oandapy.API(environment='practice',
                     access_token=config['oanda']['access_token'])
 
+# Loads Oanda trade history to data object with parameters below
+data = oanda.get_history(instrument='EUR_USD', # instrument
+                         start='2017-02-13',  # start date
+                         end='2017-02-20',  # end date
+                         granularity='M1')  # minute bars
+
+# loads Oanda data to pandas DataFrame and sets time as index
+df = pandas.DataFrame(data['candles']).set_index('time')
+
 
 
 
